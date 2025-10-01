64e20da6f338c7f448ef29050e671ba38e2eaca8
Aniket-1711 <gawandeaniketh@gmail.com>
2025-10-01 14:25:04 +0530

    feat: Initial project setup with Git LFS

---
+++ main.py
@@ -0,0 +1,11 @@
+#core libraries for App and Data Handling
+import streamlit as st
+import pandas as pd 
+import numpy as np
+
+#Libraries for data Visualization
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+#Libraries from Scikit - Learn for machine learning --> use pip install scikit-learn while installing in terminal
